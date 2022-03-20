dfbb950e7085086815c6a54380ab8ba3a7aab09d
OsbornDelAngel <ozdelangel@gmail.com>
2022-03-19 17:53:18 -0700

    permissions and postgress intergration

--- fighters/views.py
+++ fighters/views.py
@@ -2,14 +2,17 @@
 from rest_framework import generics
 from .serializer import FightersSerializer
 from .models import Fighters
+from .permissions import IsOwnerOrReadOnly
 
 class FightersList(generics.ListCreateAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     # bring in a query set. what are we going to be asking about?
     queryset = Fighters.objects.all()
     # where do we send this conversion data?
     serializer_class = FightersSerializer
 
 class FightersDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Fighters.objects.all()
     serializer_class = FightersSerializer
 
